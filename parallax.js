1669f8bf06750bfc336e67a25fa69dd8cf772e34
s1rious <58982535+s1rious@users.noreply.github.com>
2022-06-15 16:05:10 +0300

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -1,1 +1,13 @@
+document.addEventListener('mousemove', parallaxMainPictures)
 
+function parallaxMainPictures(e) {
+   if (scrollY < document.querySelector('.main-screen').offsetHeight) {
+      this.querySelectorAll('.main-screen__image').forEach(picture => {
+         const speed = picture.getAttribute('data-speed')
+         const x = (window.innerWidth - e.pageX * speed) / 100
+         const y = (window.innerHeight - e.pageY * speed) / 100
+
+         picture.style.transform = `translateX(${x}px) translateY(${y}px)`
+      })
+   }
+}
